ba1df19a4c9ee48601472449e213d45d84c59ed6
chengccj <cheng16394@gmail.com>
2019-05-11 23:59:52 +0800

    创建

---
+++ app/src/main/java/com/example/sise/util/MyCookieJar.java
@@ -0,0 +1,29 @@
+package com.example.sise.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
+import okhttp3.HttpUrl;
+
+public class MyCookieJar implements CookieJar {
+    private static List<Cookie> cookies;
+
+    @Override
+    public void saveFromResponse(HttpUrl url, List<Cookie> cookies) {
+        this.cookies = cookies;
+    }
+
+    @Override
+    public List<Cookie> loadForRequest(HttpUrl url) {
+        if (null != cookies){
+            return cookies;
+        } else {
+            return new ArrayList<Cookie>();
+        }
+    }
+    public static void resetCookies(){
+        cookies = null;
+    }
+}
